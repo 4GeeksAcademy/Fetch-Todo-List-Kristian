ec41cc1aa5d43e235311aa27f3718034d4fdcf9b
Kristian Pacheco <kristianpachecar18@gmail.com>
2025-05-05 20:35:49 +0000

    Added style and some logic

--- src/js/components/TodoList.jsx
+++ src/js/components/TodoList.jsx
@@ -1,20 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
-//create your first component
 const TodoList = () => {
-	return (
-		<div className="text-center">
-            
+	const [task, setTask] = useState("");
+	const [list, setList] = useState([]);
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	const handleDelete = (indexToDelete) => {
+		const updatedList = list.filter((_, index) => index !== indexToDelete);
+		setList(updatedList);
+	}
+
+	return (
+		<div>
+			<form
+				onSubmit={(e) => {
+					e.preventDefault();
+					if (task.trim() === "") return;
+					setList([...list, task]);
+					setTask("");
+				}}
+			>
+				<h1 className="d-flex justify-content-center">Todo List!</h1>
+				<div className="kris-card">
+					<input type="text" placeholder="What do you need to be done?" value={task} onChange={(e) => setTask(e.target.value)} />
+					<ul>
+						{list.map((item, index) => (
+							<li key={index} className="kris-li">
+								{item} <button type="button" className="float-end kris-button" value={task} onClick={() => handleDelete(index)}>X</button>
+							</li>
+						))}
+						<li className="kris-li">{list.length} item left</li>
+					</ul>
+				</div>
+			</form>
 		</div>
 	);
 };
